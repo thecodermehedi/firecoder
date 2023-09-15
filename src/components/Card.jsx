91819aae2ebfee04f181b8e122d08f9f24a7ad79
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-15 19:36:41 +0600

    ➕ Card Component Complete

---
+++ src/components/Card.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+const Card = ({card, handleCard}) => {
+  const {coverImage, title, description, price, credit} = card;
+  return (
+    <div className="card w-[20rem] h-[35rem] bg-white bg-opacity-10 shadow-xl rounded-xl">
+      <figure className="px-5 pt-5">
+        <img
+          src={coverImage}
+          alt="Shoes"
+          className="rounded-xl max-w-72 w-fit h-48"
+        />
+      </figure>
+      <div className="card-body px-5">
+        <h2 className="card-title text-white">{title}</h2>
+        <p className=" text-white opacity-50">{description}</p>
+        <p className="flex justify-between items-center w-full text-white">
+          <span className="font-semibold opacity-50"> Price: {price} BDT
+          </span>
+          <span className="font-semibold opacity-50">Credit: {credit}Hr</span>
+        </p>
+        <div className="card-actions w-full">
+          <button
+            onClick={() => handleCard(card)}
+            className="btn border-none bg-red-500 w-full min-h-0 h-10 capitalize font-bold text-lg hover:text-white text-gray-800 hover:bg-purple-700 hover:drop-shadow-[0_0_7px_rgba(168,85,247,0.5)] transition-all hover:border-transparent shadow-xl "
+          >
+            Select
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  card: PropTypes.object,
+  handleCard: PropTypes.func,
+};
+
+export default Card;
