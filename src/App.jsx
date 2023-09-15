880978bbd1798f7cb3ac7344a36ca5c242c36d7c
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-15 19:34:15 +0600

    ➕ Added: react-toastify package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Header from "./components/Header";
-
+import Main from "./components/Main";
+import {ToastContainer, toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
       <div className="bg-[#0E1316] min-h-screen ">
         <Header />
+        <ToastContainer
+          position="top-right"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <Main toast={toast} />
       </div>
     </>
   );
