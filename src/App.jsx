0367ad42c697cf2fbaed9a6b3fb514a8c1e94cad
Mehedi Hasan <devbytemehedi@gmail.com>
2023-09-17 03:41:24 +0600

    🐛 preloader bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
+import {useEffect, useState} from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {Circle} from "react-preloaders";
+import Loading from "./components/Loading";
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
-      <div className="bg-[#0E1316] min-h-screen ">
-        <Header />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss={false}
-          draggable={false}
-          pauseOnHover={false}
-          theme="dark"
-        />
-        <Main toast={toast} />
-        <Footer />
-      </div>
-      <Circle background="#0E1316" color="red" />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="bg-[#0E1316] min-h-screen ">
+          <Header />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable={false}
+            pauseOnHover={false}
+            theme="dark"
+          />
+          <Main toast={toast} />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
